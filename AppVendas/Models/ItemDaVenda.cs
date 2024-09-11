d432457e00032e4c3c81d11e24f58b8b72e652ce
ThiagoVaucci01 <thiago.dev24@gmail.com>
2024-09-11 17:03:34 -0300

    Classe criadas

---
+++ AppVendas/Models/ItemDaVenda.cs
@@ -0,0 +1,21 @@
+﻿namespace AppVendas.Models
+{
+    public class ItemDaVenda
+    {
+        public Guid ItemDaVendaId { get; set; }
+
+        //RELACIONAMENTO COM A TABELA PRODUTOS\\
+
+        public Guid VendaId { get; set; }
+        public Venda? Venda { get; set; }
+
+        //RELACIONAMENTO COM A TABELA PRODUTOS\\
+
+        public Guid ProdutoId { get; set; }
+        public Produto? Produto { get; set; }
+
+        public double QtdadeVendida { get; set; }
+        public double ValorTotalDoItem { get; set; }
+
+    }
+}
---
+++ AppVendas/Models/Venda.cs
@@ -0,0 +1,15 @@
+﻿namespace AppVendas.Models
+{
+    public class Venda
+    {
+        public Guid VendaId { get; set; }
+        public int NotaFiscal { get; set; }
+        public DateTime Datavenda { get; set; }
+        public double? TotalVenda { get; set; }
+
+        //REFERENCIA PARA A MODEL CLIENTE\\
+
+        public Guid ClienteId { get; set; }
+        public Cliente? Cliente { get; set; }
+    }
+}
